30bc5dad0f1bbb5c122befcf99a64a044282a5a9
haidang23675 <phuonguyen0623@gmail.com>
2015-10-15 21:53:57 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://xenangchaua.com/' target='_blank'><img src='//c2.staticflickr.com/6/5709/22193755375_094c6aa10d_z.jpg' width='500px'/>Xe nâng tay điện</a>. Xe nâng hàng nhập khẩu các loại. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://xenangchaua.com/' target='_blank'><img src='//c2.staticflickr.com/6/5709/22193755375_094c6aa10d_z.jpg' width='500px'/><br/>Xe nâng tay điện</a>. Xe nâng hàng nhập khẩu các loại. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
